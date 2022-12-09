22bf9f8e459fd400d4793e1d5ceb430f29e191e7
Arnar Kári <arnarkari93@gmail.com>
2022-12-09 20:48:25 +0000

    day3a

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+fn char_to_prioritie(c: &u8) -> i32 {
+    if *c >= b'a' {
+        println!("found {}: {}", *c as char, (c - b'a') as i32);
+        return (c - b'a') as i32 + 1;
+    }
+    println!("found {}: {}", *c as char, (c - b'A') as i32 + 27);
+    return (c - b'A') as i32 + 27;
 }
+
+fn main() {
+    let ruckstack = include_str!("../input.txt").lines();
+    let result: i32 = ruckstack
+        .map(|r| {
+            let (r1, r2) = r.split_at(r.len() / 2);
+            println!("r1: {}, r2: {}", r1, r2);
+            for item in r1.as_bytes() {
+                if r2.as_bytes().contains(item) {
+                    return char_to_prioritie(item);
+                }
+            }
+            return 0;
+        })
+        .sum();
+
+    println!("Result sum: {}", result);
+}
+
+    
+
+        
+
+        
