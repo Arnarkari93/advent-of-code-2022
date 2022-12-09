e2e5bcfbddab6167a1cfc42582a4d5d6e7121327
Arnar Kári <arnarkari93@gmail.com>
2022-12-09 21:18:21 +0000

    day3 generic function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,21 @@
+use std::cmp::PartialEq;
+
+fn contains_same_element<'element, T>(
+    list1: &'element [T],
+    list2: &[T],
+) -> Result<&'element T, &'static str>
+where
+    T: PartialEq,
+{
+    for element in list1 {
+        if list2.contains(element) {
+            let result = element.to_owned();
+            return Ok(result);
+        }
+    }
+    return Err("No common element found");
+}
+
 fn char_to_prioritie(c: &u8) -> i32 {
     if *c >= b'a' {
         println!("found {}: {}", *c as char, (c - b'a') as i32);
@@ -10,23 +28,15 @@ fn char_to_prioritie(c: &u8) -> i32 {
 fn main() {
     let ruckstack = include_str!("../input.txt").lines();
     let result: i32 = ruckstack
-        .map(|r| {
-            let (r1, r2) = r.split_at(r.len() / 2);
+        .map(|r| r.split_at(r.len() / 2))
+        .map(|(r1, r2)| {
             println!("r1: {}, r2: {}", r1, r2);
-            for item in r1.as_bytes() {
-                if r2.as_bytes().contains(item) {
-                    return char_to_prioritie(item);
-                }
-            }
-            return 0;
+            return match contains_same_element(r1.as_bytes(), r2.as_bytes()) {
+                Ok(c) => char_to_prioritie(c),
+                Err(_) => 0,
+            };
         })
         .sum();
 
     println!("Result sum: {}", result);
 }
-
-    
-
-        
-
-        
