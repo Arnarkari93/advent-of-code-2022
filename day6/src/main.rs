8f25b827d6e426ad072565354af84187fa429cdd
Arnar Kári <arnarkari93@gmail.com>
2022-12-15 22:38:41 +0000

    day6 b

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -8,20 +8,25 @@ fn all_uniq(s: &[char]) -> bool {
     chars.len() == s.len()
 }
 
-fn recursion_ftw(mut input: VecDeque<char>, index: usize) -> Option<usize> {
-    if input.len() < 4 {
+fn recursion_ftw(mut input: VecDeque<char>, index: usize, message_size: usize) -> Option<usize> {
+    if input.len() < message_size + 1 {
         return None;
     }
     let next_index = index + 1;
 
-    let (a, b, c, d) = (input.pop_front().unwrap(), input[0], input[1], input[2]);
-    println!("Step:  {}{}{}{}", a, b, c, d);
+    let front = input.pop_front().unwrap();
+    let mut rest = input
+        .range(0..(message_size - 1))
+        .copied()
+        .collect::<VecDeque<char>>();
+    let mut message = VecDeque::from([front]);
+    message.append(&mut rest);
 
-    if all_uniq(&[a, b, c, d]) {
+    if all_uniq(&message.make_contiguous()) {
         return Some(next_index);
     }
 
-    return recursion_ftw(input, next_index);
+    return recursion_ftw(input, next_index, message_size);
 }
 
 fn main() {
@@ -29,8 +34,11 @@ fn main() {
 
     for line in input_lines {
         let input = line.chars();
-        println!("input: {}", line);
-        let output = recursion_ftw(VecDeque::from_iter(input.into_iter()), 3);
-        println!("Result {}", output.unwrap_or(0));
+
+        let result_a = recursion_ftw(VecDeque::from_iter(input.clone().into_iter()), 3, 4);
+        let result_b = recursion_ftw(VecDeque::from_iter(input.clone().into_iter()), 13, 14);
+
+        println!("Result A:{}", result_a.unwrap_or(0));
+        println!("Result B:{}", result_b.unwrap_or(0));
     }
 }
